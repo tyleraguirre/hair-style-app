653d60b9f6d5849504e222ea01966983bc8a1648
SpencerNinja <spencer.ninja@gmail.com>
2021-09-02 15:22:58 -0600

    added csv file in raw and res folders

--- HairCutApp/app/src/main/java/com/example/haircutapp/MainActivity.kt
+++ HairCutApp/app/src/main/java/com/example/haircutapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.haircutapp
 
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +10,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.haircutapp.databinding.ActivityMainBinding
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import java.io.File
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +36,35 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+//        csvReader().open("stylesDatabaseAst.csv") {
+//            readAllWithHeaderAsSequence().forEach { row: Map<String, String> ->
+//                //Do something
+//                println(row) //{id=1, name=doyaaaaaken}
+//                Log.i("test2", "${row}")
+//            }
+//        }
+
+        val readTxt = csvReader().open("src/main/assets/stylesDatabaseAst.csv") {
+            readAllAsSequence().forEach { row ->
+                println(row)
+                Log.v("styleDB", "${row}")
+            }
+        }
+        println(readTxt)
+
+        try {
+            val readTxt = csvReader().open("src/main/res/styles_database_res.csv") {
+                readAllAsSequence().forEach { row ->
+                    println(row)
+                    Log.i("styleDB", "${row}")
+                }
+            }
+        } catch (exception: NumberFormatException) {
+            println(exception)
+        }
+
+
+
     }
 }
