9a4bae43642147c5423c5c96e217494e6c740987
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2021-09-08 16:13:23 -0600

    have a GoogleMap populating on the mapFragment

--- HairCutApp/app/src/main/java/com/example/haircutapp/MainActivity.kt
+++ HairCutApp/app/src/main/java/com/example/haircutapp/MainActivity.kt
@@ -40,129 +40,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.bottomNavview
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_styles, R.id.navigation_favorites, R.id.navigation_search, R.id.navigation_map
+                R.id.navigation_styles,
+                R.id.navigation_favorites,
+                R.id.navigation_search,
+                R.id.navigation_map
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-    private fun checkMapServices(): Boolean {
-        if (isServicesOK()) {
-            if (isMapsEnabled()) {
-                return true
-            }
-        }
-        return false
-    }
-
-    private fun buildAlertMessageNoGps() {
-        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
-            .setCancelable(false)
-            .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
-                val enableGpsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS)
-            })
-        val alert: AlertDialog = builder.create()
-        alert.show()
-    }
-
-    fun isMapsEnabled(): Boolean {
-        val manager = getSystemService<Any>(Context.LOCATION_SERVICE) as LocationManager
-        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            buildAlertMessageNoGps()
-            return false
-        }
-        return true
-    }
-
-    private fun getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-        if (ContextCompat.checkSelfPermission(
-                this.applicationContext,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            myLocationPermissionGranted = true
-        } else {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
-        }
-    }
-
-    fun isServicesOK(): Boolean {
-        Log.d(TAG, "isServicesOK: checking google services version")
-        val available =
-            GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this@MainActivity)
-        if (available == ConnectionResult.SUCCESS) {
-            //everything is fine and the user can make map requests
-            Log.d(TAG, "isServicesOK: Google Play Services is working")
-            return true
-        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
-            //an error occured but we can resolve it
-            Log.d(TAG, "isServicesOK: an error occured but we can fix it")
-            val dialog: Dialog = GoogleApiAvailability.getInstance()
-                .getErrorDialog(this@MainActivity, available, ERROR_DIALOG_REQUEST)
-            dialog.show()
-        } else {
-            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show()
-        }
-        return false
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String?>,
-        grantResults: IntArray
-    ) {
-        myLocationPermissionGranted = false
-        when (requestCode) {
-            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.size > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
-                    myLocationPermissionGranted = true
-                }
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.d(TAG, "onActivityResult: called.")
-        when (requestCode) {
-            PERMISSIONS_REQUEST_ENABLE_GPS -> {
-                if (myLocationPermissionGranted) {
-
-                } else {
-                    getLocationPermission()
-                }
-            }
-        }
-    }
-    override fun onResume() {
-        super.onResume()
-        if (checkMapServices()) {
-            if (myLocationPermissionGranted) {
-            } else {
-                getLocationPermission()
-            }
-        }
     }
 }
