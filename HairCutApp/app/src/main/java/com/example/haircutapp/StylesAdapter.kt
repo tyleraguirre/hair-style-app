961be61104a5757008dd811839a24df83288b942
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2021-09-14 12:53:39 -0600

    setting up the binding adapter

--- HairCutApp/app/src/main/java/com/example/haircutapp/StylesAdapter.kt
+++ HairCutApp/app/src/main/java/com/example/haircutapp/StylesAdapter.kt
@@ -2,15 +2,60 @@ package com.example.haircutapp
 
 import android.app.Activity
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.haircutapp.databinding.ListItemHairstyleBinding
 import com.example.haircutapp.hairstylesdatabase.Hairstyle
+import com.example.haircutapp.ui.styles.StylesViewModel
 
-class StylesAdapter (val activity: Activity, val stylesList: List<Hairstyle>):
-        RecyclerView.Adapter<StylesAdapter.StylesViewHolder>() {
+//class StylesAdapter(val context: Context, val stylesList: List<Hairstyle>):
+//        RecyclerView.Adapter<StylesAdapter.StylesViewHolder>() {
+//
+//            inner class StylesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//                val styleName: TextView = itemView.
+//            }
+//        }
 
-            inner class StylesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-                val styleName: TextView = itemView.
+class StylesAdapter(val viewModel: StylesViewModel): ListAdapter<Hairstyle, StylesAdapter.StylesViewHolder>(StylesListDiffCallback()) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): StylesAdapter.StylesViewHolder {
+        val view = ListItemHairstyleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StylesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StylesAdapter.StylesViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class StylesViewHolder(val binding: ListItemHairstyleBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val item = getItem(position)
+
+            binding.mainLayout.setOnClickListener {
+                viewModel.setHairstyle(item)
             }
+            var hairstyleName = binding.styleName
+            var hairstyleImage = binding.styleImage
         }
+    }
+
+    class StylesListDiffCallback : DiffUtil.ItemCallback<Hairstyle>() {
+
+        override fun areItemsTheSame(oldItem: Hairstyle, newItem: Hairstyle): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Hairstyle, newItem: Hairstyle): Boolean {
+            return oldItem.hairstyleId == newItem.hairstyleId
+        }
+    }
+}
--- HairCutApp/app/src/main/java/com/example/haircutapp/ui/styles/StylesViewModel.kt
+++ HairCutApp/app/src/main/java/com/example/haircutapp/ui/styles/StylesViewModel.kt
@@ -4,12 +4,23 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.haircutapp.hairstylesdatabase.Hairstyle
 import com.example.haircutapp.hairstylesdatabase.HairstyleDao
 import com.example.haircutapp.hairstylesdatabase.HairstyleDatabase
 
 class StylesViewModel(val database: HairstyleDao, application: Application) : ViewModel() {
 
-    val activities = database.getAllHairstyles()
+    private val _selectedStyle = MutableLiveData<Hairstyle?>()
+    val selectedStyle: LiveData<Hairstyle?>
+    get() = _selectedStyle
+
+    fun navigationComplete() {
+        _selectedStyle.value = null
+    }
+
+    fun setHairstyle(hairstyle: Hairstyle) {
+        _selectedStyle.value = Hairstyle()
+    }
 //    private val _text = MutableLiveData<String>().apply {
 //        value = "This is styles Fragment"
 //    }
