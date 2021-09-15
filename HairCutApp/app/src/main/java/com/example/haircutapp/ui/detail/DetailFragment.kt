8de7a0444d116adb6bf833d0a1324bce6bd631b3
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2021-09-15 12:59:47 -0600

    have the recyclerview displaying data

--- HairCutApp/app/src/main/java/com/example/haircutapp/ui/detail/DetailFragment.kt
+++ HairCutApp/app/src/main/java/com/example/haircutapp/ui/detail/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.haircutapp.R
 import com.example.haircutapp.databinding.FragmentDetailBinding
 import com.example.haircutapp.hairstylesdatabase.HairstyleDatabase
