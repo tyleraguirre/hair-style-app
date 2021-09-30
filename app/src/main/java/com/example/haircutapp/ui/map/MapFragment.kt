8efadc7c249bda97c83fb550ac93c7126ce8ac1b
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2021-09-30 16:40:10 -0600

    following codingWithMitch github

--- app/src/main/java/com/example/haircutapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/haircutapp/ui/map/MapFragment.kt
@@ -1,26 +1,29 @@
 package com.example.haircutapp.ui.map
 
-import android.app.Activity.RESULT_OK
+//import com.example.haircutapp.databinding.FragmentMapBinding
+//import com.google.android.gms.location.places.ui.PlacePicker
+
 import android.content.pm.PackageManager
 import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.haircutapp.R
-//import com.example.haircutapp.databinding.FragmentMapBinding
-import android.location.Geocoder
-import android.util.Log
-import android.widget.SearchView
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException
-import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.example.haircutapp.databinding.FragmentMapBinding
+import com.example.haircutapp.databinding.FragmentStylesBinding
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.places.ui.PlacePicker
-//import com.google.android.gms.location.places.ui.PlacePicker
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -31,12 +34,13 @@ import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_map.*
 import java.io.IOException
 
-private lateinit var map: GoogleMap
-private lateinit var fusedLocationClient: FusedLocationProviderClient
-private lateinit var lastLocation: Location
-
 
 class MapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
+
+    private lateinit var binding: FragmentMapBinding
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
@@ -104,8 +108,19 @@ class MapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListen
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false)
+        val autocompleteFragment = PlaceAutocompleteFragment().fragmentManager.findFragmentById(R.id.map_fragment)
 
+       autocompleteFragment!!.onPlaceSelected(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                map.clear()
+                map.addMarker(MarkerOptions().position(place.latLng).title(place.name.toString()))
+                map.moveCamera(CameraUpdateFactory.newLatLng(place.latLng))
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(place.latLng, 12.0f))
+            }
+
+            override fun onError(status: Status?) {}
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -113,27 +128,27 @@ class MapFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListen
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
     }
 
 
-    private fun searchQuery() {
-        val searchView = search_view
-        var location = searchView.query.toString()
-        var searchAddressList = listOf<Address?>()
-
-        if (location != null || location.equals("")) {
-            val geocoder: Geocoder
-
-            try {
-                searchAddressList = geocoder.getFromLocationName(location,1)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            searchAddressList.get(0)
-            searchAddressList.
-        }
-    }
+//    private fun searchQuery() {
+//        val searchView = search_view
+//        var location = searchView.query.toString()
+//        var searchAddressList = listOf<Address?>()
+//
+//        if (location != null || location.equals("")) {
+//            val geocoder: Geocoder
+//
+//            try {
+//                searchAddressList = geocoder.getFromLocationName(location,1)
+//            } catch (e: IOException) {
+//                e.printStackTrace()
+//            }
+//            searchAddressList.get(0)
+//            searchAddressList.
+//        }
+//    }
 
 
 
